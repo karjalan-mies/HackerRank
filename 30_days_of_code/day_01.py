94b8586831b4256e24fda66008b45590ef460f55
Denis Trifonov <trifonov.d.a@yandex.ru>
2021-05-05 20:29:13 +0300

    Задание 01 дня челенджа и задача на списки из практики по Python

---
+++ 30_days_of_code/day_01.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+_i = int(input())
+_f = float(input())
+_s = str(input())
+
+
+# Print the sum of both integer variables on a new line.
+print(i + _i)
+# Print the sum of the double variables on a new line.
+print(d + _f)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + _s)
---
+++ python_practice/print_function.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    if 0 < n < 151:
+        for i in range(n):
+            print(i+1, end='')
